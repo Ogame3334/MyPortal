c0f9276354a7202968425f44a906852343438778
Ogame3334 <kazuhiroisobe0522@gmail.com>
2023-05-03 04:17:13 +0900

    検索欄を追加、ボードの配置を変更

---
+++ script.js
@@ -0,0 +1,20 @@
+function gen_search_url(words){
+    const base_url = 'https://www.google.com/search?q=';
+    words = base_url + words;
+    words = words.replaceAll('+', '%2B').replaceAll(' ', '+').replaceAll('　', '+');
+    url = words + '&hl=ja';
+
+    return url;
+}
+
+function search(){
+    const words = document.getElementById('search-text-box').value;
+    const url = gen_search_url(words);
+    window.location.href = url;
+}
+
+function down_enter(code){
+    if(13 == code){
+        search();
+    }
+}
